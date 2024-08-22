1677708d0e79f4cada4736e4b83040d6f52c8445
voweiro <ajenaghonorevoweiro@gmail.com>
2024-08-22 01:57:59 +0100

    well-done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+function App() {
+
+  const routes = createBrowserRouter ()
+  
+  return (
+    <>
+    <rout
+     
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Root.js
@@ -0,0 +1,9 @@
+
+
+export default function Root() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
